15e51521618ab193b07ef562b8684d2fc28294c4
Jarmir Smith <55027346+jarmirs@users.noreply.github.com>
2022-08-28 22:35:09 -0400

    2nd comm

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,14 +1,34 @@
 import React from "react";
 import "./Feed.css";
-import{TwitterTimelineEmbed,
-TwitterShareButton,
-TwitterTweetEmbed} from 'react-twitter-embed';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton,
+} from "react-twitter-embed";
 
 
 function Widgets(){
     return(
 <div className="widgets">
-   
+    <div className= "widgets-SearchIcon">
+<input className="form-control mr-2" type="search" placeholder="Search Twitter" aria-label="Search"></input>
+    </div>
+   <div className="widgets-Container">
+    <h2>What's happening</h2>
+    <TwitterTweetEmbed tweetId=""
+    sourceType= "profile"
+    screenName=""
+    options={{heights: 400}} />
+
+    <TwitterShareButton/>
+   </div>
 </div>
     );
 }
